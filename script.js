cd50d8c81cfe15e66aea09ccc76f1a764b9f25ef
Carlos Salazar <carlosiusalazar@gmail.com>
2023-03-21 14:34:20 +0900

    initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const form = document.getElementById('chat-form');
+const mytextInput = document.getElementById('mytext');
+const responseTextarea = document.getElementById('response');
+
+const API_KEY = 'Paste-Your-API-Key-Here';
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const mytext = mytextInput.value.trim();
+
+    if (mytext) {
+        try {
+            const response = await fetch('https://api.openai.com/v1/chat/completions', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${API_KEY}`,
+                },
+                body: JSON.stringify({
+                    model: 'gpt-4',
+                    messages: [{ role: 'user', content: mytext }],
+                    temperature: 1.0,
+                    top_p: 0.7,
+                    n: 1,
+                    stream: false,
+                    presence_penalty: 0,
+                    frequency_penalty: 0,
+                }),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                responseTextarea.value = data.choices[0].message.content;
+            } else {
+                responseTextarea.value = 'Error: Unable to process your request.';
+            }
+        } catch (error) {
+            console.error(error);
+            responseTextarea.value = 'Error: Unable to process your request.';
+        }
+    }
+});
